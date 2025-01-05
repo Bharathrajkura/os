12e76c175a138d30d4fb692c1c6fa4701135c88b
Bharath Raj Kura <162363639+Bharathrajkura@users.noreply.github.com>
2025-01-05 19:31:31 +0530

    Update roundRobin.c

--- roundRobin.c
+++ roundRobin.c
@@ -1,45 +1,58 @@
 #include<stdio.h>
-int main(){
-    int n,bt[20],wt[20],tat[20],remaining_bt[20],t;
-    float atat=0,awt=0;
-    int ellapsed_time=0;
-    printf("Enter the Number of Processes : ");
-    scanf("%d",&n);
-    for(int i=0;i<n;i++){
-        printf("Enter the Burst Time for Process %d : ",i);
-        scanf("%d",&bt[i]);
-        remaining_bt[i]=bt[i];
+#include<conio.h>
+
+void main() {
+    int i, j, n, bu[10], wa[10], tat[10], t, ct[10], max;
+    float awt = 0, att = 0, temp = 0;
+
+    clrscr();
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++) {
+        printf("\nEnter Burst Time for process %d: ", i+1);
+        scanf("%d", &bu[i]);
+        ct[i] = bu[i];
     }
-    printf("Enter the size of time sliced : ");
-    scanf("%d",&t);
-    int processes_left=n;
-    while(processes_left!=0){
-        for(int i=0;i<n;i++){
-            if(remaining_bt[i]>0){
-                if(remaining_bt[i]<=t){
-                    ellapsed_time+=remaining_bt[i];
-                    tat[i]=ellapsed_time;
-                    remaining_bt[i]=0;
-                    processes_left--;
-                }else{
-                    ellapsed_time+=t;
-                    remaining_bt[i]-=t;
+
+    printf("\nEnter the size of time slice: ");
+    scanf("%d", &t);
+
+    max = bu[0];
+    for(i = 1; i < n; i++) {
+        if(max < bu[i]) {
+            max = bu[i];
+        }
+    }
+
+    for(j = 0; j < (max/t) + 1; j++) {
+        for(i = 0; i < n; i++) {
+            if(bu[i] != 0) {
+                if(bu[i] <= t) {
+                    tat[i] = temp + bu[i];
+                    temp = temp + bu[i];
+                    bu[i] = 0;
+                } else {
+                    bu[i] = bu[i] - t;
+                    temp = temp + t;
                 }
             }
         }
     }
-    for(int i=0;i<n;i++){
-        wt[i]=tat[i]-bt[i];
-        atat+=tat[i];
-        awt+=wt[i];
-    }
-    printf("\nThe Average Turnaround Time is: %.2f", atat / n);
-    printf("\nThe Average Waiting Time is: %.2f", awt / n);
 
-    printf("\n\nPROCESS\tBURST TIME\tWAITING TIME\tTURNAROUND TIME\n");
-    for (int i = 0; i < n; i++) {
-        printf("P%d\t%d\t\t%d\t\t%d\n", i, bt[i], wt[i], tat[i]);
+    for(i = 0; i < n; i++) {
+        wa[i] = tat[i] - ct[i];
+        att += tat[i];
+        awt += wa[i];
     }
-    return 0;
 
+    printf("\nThe Average Turnaround time is: %f", att/n);
+    printf("\nThe Average Waiting time is: %f", awt/n);
+    printf("\n\tPROCESS\t BURST TIME \t WAITING TIME\t TURNAROUND TIME\n");
+
+    for(i = 0; i < n; i++) {
+        printf("\t%d \t %d \t\t %d \t\t %d \n", i+1, ct[i], wa[i], tat[i]);
+    }
+
+    getch();
 }
